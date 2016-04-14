9cfb4941665b854299541ffa911f55512cd5cc18
Guilherme Emmanuel <guilherme1.0emmanuel@gmail.com>
2016-04-14 03:24:25 -0300

    gulp bower npm

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # StorageExample
 Example in AngularJS
+
+
+Instructions to Run:
+
+1. Install NodeJs
+2. execute npm install -g gulp
+3. In Project Folder execute npm install gulp
+4. execute npm install
+5. execute gulp bower
+6. execute gulp run-dist
+7. access System in displayed link
--- js/controllers/contactCtrl.js
+++ js/controllers/contactCtrl.js
@@ -1,9 +1,26 @@
 angular.module("storageExample").controller("contactCtrl", function($scope) {
-	$scope.contacts = [
-		{name:"Jonh", email:"jonh@emb.com", phone:"(342) 988-8923"}
-	];
+	if(typeof(Storage) !== "undefined") {
+		console.log("Your browser supports local Storage");
+				
+		if(localStorage.contacts == undefined) {
+			localStorage.contacts = JSON.stringify([]);
+			$scope.contacts = JSON.parse(localStorage.getItem('contacts'));
+			console.log("Creation of contacts");			
+		}
+		else {		
+			$scope.contacts = JSON.parse(localStorage.getItem('contacts'));
+			console.log("Retrieve of Contacts");
+					
+		}
+				
+	} else {
+		console.log("Your browser does not support local Storage");
+	}
+	
 	$scope.addContact = function(contact) {
+		$scope.contacts = JSON.parse(localStorage.getItem('contacts'));
 		$scope.contacts.push(angular.copy(contact));
+		localStorage.setItem("contacts", JSON.stringify($scope.contacts));
 		$scope.eraseContactForm();
 		
 	}
